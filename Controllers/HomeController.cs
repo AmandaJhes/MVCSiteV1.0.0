9677dd6f198449ae94a72ba1f49ff9b90d9fb8d4
Amanda Nascimento <87491868+AmandaJhes@users.noreply.github.com>
2023-04-24 00:25:29 -0300

    project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVCSite.Models;
+
+namespace MVCSite.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        HomeModel home = new HomeModel();
+        home.Nome = "Amanda Nascimento";
+        home.Email = "amanda.nascimento@email.com";
+        return View(home);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
